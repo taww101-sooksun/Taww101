ac7e3879e06ef5c50fe68e61821bdb66000d2205
taww101-sooksun <taww101@gmail.com>
2026-02-22 05:23:36 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -63,7 +63,10 @@ st.markdown("""
     h1, h2, h3, p, span { color: white !important; }
     </style>
     """, unsafe_allow_html=True)
-
+if os.path.exists("logo2.jpg"):
+    st.image("logo2.jpg", width=300)
+else:
+    
 # --- 2. HEADER ---
 try:
     st.image("logo.jpg", width=300)
