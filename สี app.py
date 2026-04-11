a54b3f37c27ae404d54fe782b44df42e6a56d318
taww101-sooksun <taww101@gmail.com>
2026-04-12 01:14:05 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -17,7 +17,10 @@ import random
 from streamlit_js_eval import get_geolocation 
 # --- จุดสำคัญ: ต้อง Import แบบนี้เท่านั้น ---
 from streamlit_js_eval import get_geolocation 
-   if os.path.exists
+if os.path.exists("synapse.mp4"):
+    st.video("synapse.mp4")
+else:
+    st.warning("⚠️ ไม่พบไฟล์วิดีโอ synapse.mp4")   
 def apply_custom_background():
     st.markdown(
         f"""
