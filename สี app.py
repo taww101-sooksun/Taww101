01c9bcaabccd4598afae74b4399d39152dcd5b5f
taww101-sooksun <taww101@gmail.com>
2026-04-24 20:57:33 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -53,7 +53,7 @@ st.markdown(f"""
     </style>
     <img src="data:image/png;base64,{{logo_base64}}" class="global-logo">
 """, unsafe_allow_html=True)
-if selected_bg_music != "None":
+
  audio_data = get_base64_data(selected_bg_music)
 st.sidebar.markdown(f"""
         <audio id="bgAudio" autoplay loop controls style="width: 100%; height: 30px;">
