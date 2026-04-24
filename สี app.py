663329ff32292ba0d9b23862f1dcd3e0f08c4bae
taww101-sooksun <taww101@gmail.com>
2026-04-24 19:39:19 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -803,4 +803,4 @@ elif st.session_state.page == "10":
 if 'primary_color' not in st.session_state:
     st.session_state.primary_color = "#00f3ff"
 if 'page' not in st.session_state:
-    st.session_state.page = "HOME”
+    st.session_state.page = "HOME"
