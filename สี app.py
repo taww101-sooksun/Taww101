c9fd39b76952de1cc22bea21c145af74d4fee048
taww101-sooksun <taww101@gmail.com>
2026-04-25 08:45:42 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -341,8 +341,3 @@ with st.container():
                 st.rerun() 
 
 
-
-ModuleNotFoundError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
-Traceback:
-File "/mount/src/taww101/ส
-   
