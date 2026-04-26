9f1de135c9fef33b5d8a714f9004e2c86b4db2a6
taww101-sooksun <taww101@gmail.com>
2026-04-27 02:06:19 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -255,7 +255,7 @@ else:
     # 📥 โซนจัดการห้องรบ (UNIT 01 - 11)
     # =========================================================
 
-       if st.session_state.page == "1":
+        if st.session_state.page == "1":
         # 1. หัวข้อและโลโก้ (ดึงสไตล์จากหน้าหลัก)
         st.markdown("<h1 class='neon-title-unit'>🎵 UNIT 01: SYNAPSE SPECTRUM</h1>", unsafe_allow_html=True)
         
