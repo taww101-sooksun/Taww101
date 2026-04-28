f99a95ff8c7f4c81bdd79f64cf7bd9b7d2642145
taww101-sooksun <taww101@gmail.com>
2026-04-28 17:20:43 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -56,7 +56,7 @@ if loc:
     accuracy = loc['coords'].get('accuracy', 'N/A')
     st.write(f"ความคลาดเคลื่อน: {accuracy} เมตร")
     
-    if accuracy > 1000:
+    if accuracy > 10:
         st.warning("⚠️ สัญญาณ GPS ไม่แม่นยำ (อาจเคลื่อนได้เป็นกิโล) ลองออกไปที่โล่งครับ")
 
 
