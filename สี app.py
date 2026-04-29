d08033d44c0d0370ec0ab2368548180bf62f55e3
taww101-sooksun <taww101@gmail.com>
2026-04-30 00:30:25 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -60,7 +60,7 @@ def room_radar():
     all_users = db.reference('users').get()
     
     start_lat, start_lon = 13.7367, 100.5231
-    if loc:
+if loc:
     start_lat = loc['coords']['latitude']
     start_lon = loc['coords']['longitude']
     else:
