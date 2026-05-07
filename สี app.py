5b1ce9c8892cdac9acd00040080d532737518255
taww101-sooksun <taww101@gmail.com>
2026-05-07 23:31:18 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -28,7 +28,7 @@ st.markdown(f"""
 
     .logo-container {{ display: flex; justify-content: center; margin-top: 20px; }}
     .neon-logo {{
-        width: 300px; height: 300px;
+        width: 150px; height: 1500px;
         background-image: url("{logo_html_link}");
         background-size: contain; background-repeat: no-repeat;
         filter: drop-shadow(0 0 10px #00f3ff);
