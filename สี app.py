fb57ea2d3e526b6936925591f0a50edae7f4de14
taww101-sooksun <taww101@gmail.com>
2026-05-19 20:59:35 +0700

    Update สี app.py

--- สี app.py
+++ สี app.py
@@ -179,7 +179,9 @@ elif menu_choice == "🛰️ GPS TRACER":
         # วาดแผนที่ Folium จริงๆ
         m = folium.Map(location=[lat, lon], zoom_start=15, tiles="CartoDB dark_matter")
         folium.Marker([lat, lon], popup="พิกัดปัจจุบันของคุณ", icon=folium.Icon(color="green")).add_to(m)
-        st_folium(m, width=requests=None, height=400)
+    # แก้ไขจากของเดิมที่เป็น Error ให้เหลือแค่นี้พอครับ ทำงานได้จริงแน่นอน
+st_folium(m, width="100%", height=400)
+
     else:
         st.warning("🔄 กำลังรอการตอบรับพิกัด หรือบราว์เซอร์ของคุณไม่ได้เปิดแชร์ตำแหน่งโลเคชั่น")
 
