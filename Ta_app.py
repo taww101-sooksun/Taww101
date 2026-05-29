7cadc5abdb758b594636bd8e6bbea127eb48bc64
taww101-sooksun <taww101@gmail.com>
2026-05-30 02:00:08 +0700

    Update Ta_app.py

--- Ta_app.py
+++ Ta_app.py
@@ -1,87 +1,103 @@
-# -*- coding: utf-8 -*-
-"""Setup module."""
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# -*- coding:utf-8 -*-
+
+import json
+
+from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi.middleware.cors import CORSMiddleware
+
+import schemas
+from deps import get_token
+from utils import generate_lyrics, generate_music, get_feed, get_lyrics, get_credits
+
+app = FastAPI()
 
 
-MINIMAL_DESCRIPTION = '''ASCII art is also known as "computer text art".
-    It involves the smart placement of typed special characters or
-    letters to make a visual shape that is spread over multiple lines of text.
-    ART is a Python lib for text converting to ASCII art fancy.'''
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-def get_dev_requires():
-    """Read dev-requirements.txt."""
-    requirements = open("dev-requirements.txt", "r").read()
-    return [x for x in requirements.split() if x]
+@app.get("/")
+async def get_root():
+    return schemas.Response()
 
 
-def read_description():
-    """Read README.md and CHANGELOG.md."""
+@app.post("/generate")
+async def generate(
+    data: schemas.CustomModeGenerateParam, token: str = Depends(get_token)
+):
     try:
-        with open("README.md") as r:
-            description = "\n"
-            description += r.read()
-        with open("CHANGELOG.md") as c:
-            description += "\n"
-            description += c.read()
-        return description
-    except Exception:
-        return MINIMAL_DESCRIPTION
+        resp = await generate_music(data.dict(), token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
 
-setup(
-    name='art',
-    packages=['art', 'art.data', 'art.tests'],
-    version='6.5',
-    description='ASCII Art Library For Python',
-    long_description=read_description(),
-    long_description_content_type='text/markdown',
-    author='Sepand Haghighi',
-    author_email='info@ascii-art.site',
-    url='https://github.com/sepandhaghighi/art',
-    keywords="ascii art python3 python text font non-ascii printing",
-    project_urls={
-        'Webpage': 'https://www.ascii-art.site',
-        'Source': 'https://github.com/sepandhaghighi/art',
-        'Tracker': 'https://github.com/sepandhaghighi/art/issues',
-        'Discord': 'https://discord.com/invite/FAAyq3QJqP',
-    },
-    install_requires=[],
-    extras_require={
-        "dev": get_dev_requires()
-    },
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Science/Research',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Topic :: Text Processing :: Fonts',
-        'Topic :: Text Editors',
-        'Topic :: Text Processing :: General',
-        'Topic :: Utilities',
-        'Topic :: Multimedia',
-        'Topic :: Printing',
-    ],
-    license='MIT',
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'art = art.__main__:main',
-        ]}
-    )
+@app.post("/generate/description-mode")
+async def generate_with_song_description(
+    data: schemas.DescriptionModeGenerateParam, token: str = Depends(get_token)
+):
+    try:
+        resp = await generate_music(data.dict(), token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
+@app.get("/feed/{aid}")
+async def fetch_feed(aid: str, token: str = Depends(get_token)):
+    try:
+        resp = await get_feed(aid, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
+@app.post("/generate/lyrics/")
+async def generate_lyrics_post(request: Request, token: str = Depends(get_token)):
+    req = await request.json()
+    prompt = req.get("prompt")
+    if prompt is None:
+        raise HTTPException(
+            detail="prompt is required", status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    try:
+        resp = await generate_lyrics(prompt, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
+@app.get("/lyrics/{lid}")
+async def fetch_lyrics(lid: str, token: str = Depends(get_token)):
+    try:
+        resp = await get_lyrics(lid, token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
+@app.get("/get_credits")
+async def fetch_credits(token: str = Depends(get_token)):
+    try:
+        resp = await get_credits(token)
+        return resp
+    except Exception as e:
+        raise HTTPException(
+            detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
